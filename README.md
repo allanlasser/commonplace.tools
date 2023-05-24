a9ebaeb1cc7b273e4ae67e7be0af3774d127cd3c
Allan Lasser <lasser.allan@gmail.com>
2023-05-24 16:49:13 +0000

    Basic hello-world style stuff
    - Update Readme with instructions for running SvelteKit dev mode
    - Updates skeleton page for fun

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # commonplace.tools
 Decentralized Tool Share
+
+## Frontend
+
+The frontend uses SvelteKit. It's fun and nice!
+
+### Development
+
+```sh
+cd frontend
+npm install
+# if you're running locally:
+npm run dev -- --open
+# if you're using Codespaces:
+npm run dev -- --host
+```
+
+If you're using Codespaces, using `--host` exposes the port to the network.
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
+<style>
+  .title {
+    font-size: 3em;
+    color: green;
+  }
+</style>
+
+<h1 class="title">Commonplace Tools</h1>
+<h2>Distributed toolshare</h2>
+<p>Stop buying things and leaving them in your garage all year</p>
+<p>Share them with your neighbors, instead!</p>
+
+<h3>References</h3>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
