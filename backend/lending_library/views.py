6f43b7f4ac79a00bbd4e087d08785519d1fdfd84
Chris Hartley <chris.hartley@renewindianapolis.org>
2023-06-06 15:56:00 -0400

    add default ordering to views to prevent pagination issues down the line

--- backend/lending_library/views.py
+++ backend/lending_library/views.py
@@ -8,41 +8,41 @@ from rest_framework import permissions
 from rest_framework.viewsets import ModelViewSet
 
 class LocationViewSet(ModelViewSet):
-    queryset = Location.objects.all()
+    queryset = Location.objects.all().order_by('id')
     serializer_class = LocationSerializer
 
 class NetworkViewSet(ModelViewSet):
-    queryset = Network.objects.all()
+    queryset = Network.objects.all().order_by('id')
     serializer_class = NetworkSerializer
 
 class LendableTypeViewSet(ModelViewSet):
-    queryset = LendableType.objects.all()
+    queryset = LendableType.objects.all().order_by('id')
     serializer_class = LendableTypeSerializer
 
 class LendableStatusViewSet(ModelViewSet):
-    queryset = LendableStatus.objects.all()
+    queryset = LendableStatus.objects.all().order_by('id')
     serializer_class = LendableStatusSerializer
 
 class LendableViewSet(ModelViewSet):
-    queryset = Lendable.objects.all()
+    queryset = Lendable.objects.all().order_by('id')
     serializer_class = LendableSerializer
 
 class LoanViewSet(ModelViewSet):
-    queryset = Loan.objects.all()
+    queryset = Loan.objects.all().order_by('id')
     serializer_class = LoanSerializer
 
 class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
 
 class UserViewSet(ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
 
 class GroupViewSet(ModelViewSet):
-    queryset = Group.objects.all()
+    queryset = Group.objects.all().order_by('id')
     serializer_class = GroupSerializer
 
 class PermissionViewSet(ModelViewSet):
-    queryset = Permission.objects.all()
+    queryset = Permission.objects.all().order_by('id')
     serializer_class = PermissionSerializer
