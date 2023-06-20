2b97a92e7246a78b508583a8b87e648f16ce7c47
Allan Lasser <lasser.allan@gmail.com>
2023-06-20 14:29:47 -0400

    Adds IDs to all serializers

--- backend/lending_library/serializers.py
+++ backend/lending_library/serializers.py
@@ -5,17 +5,20 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = User
 #        fields = ['first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined']
         fields = '__all__'
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Group
         fields = '__all__'
 
 class PermissionSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Permission
         fields = '__all__'
@@ -27,31 +30,37 @@ class ContentTypeSerializer(serializers.HyperlinkedModelSerializer):
         fields = '__all__'
 
 class LendableSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Lendable
         fields = '__all__'
 
 class LendableTypeSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = LendableType
         fields = '__all__'
 
 class LendableStatusSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = LendableStatus
         fields = '__all__'
 
 class NetworkSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Network
         fields = '__all__'
 
 class LoanSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Loan
         fields = '__all__'
 
 class LocationSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Location
         fields = '__all__'
