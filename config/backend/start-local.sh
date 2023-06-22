7369083be5705c72d3df7116b1f8d4d3029b79e4
Chris Hartley <chris.hartley@renewindianapolis.org>
2023-06-21 21:58:04 -0400

    add host name

---
+++ config/backend/start-local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+python /code/backend/manage.py migrate
+python /code/backend/manage.py runserver 0.0.0.0:8000
+#cd /code/backend
+#gunicorn --bind=0.0.0.0 backend.wsgi
