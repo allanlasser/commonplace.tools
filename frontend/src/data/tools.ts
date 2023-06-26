1cc3968b3c99362fd581ebfea1bfb3940b7091d2
Allan Lasser <lasser.allan@gmail.com>
2023-06-26 18:21:11 -0400

    Renders loan history on tool page
    Improve loan history

--- frontend/src/data/tools.ts
+++ frontend/src/data/tools.ts
@@ -1,24 +1,23 @@
-import { API_URL } from "@/constants";
-import { getList, getSingle } from "./api";
+import { getList, getSingle } from './api';
 
 export interface LendablesResult {
-  id: number;
-  url: string;
-  name: string;
-  owner: string;
-  created: string;
-  modified: string;
-  replacement_cost: string;
-  lendable_status: string;
-  lendable_type: string;
-  location: string | null;
-  location_updated: string | null;
+	id: number;
+	url: string;
+	name: string;
+	owner: string;
+	created: string;
+	modified: string;
+	replacement_cost: string;
+	lendable_status: string;
+	lendable_type: string;
+	location: string | null;
+	location_updated: string | null;
 }
 
 export async function getToolList() {
-  return getList<LendablesResult>(API_URL + '/lendables/?format=json');
+	return getList<LendablesResult>('lendables');
 }
 
 export async function getTool(id: string) {
-  return getSingle<LendablesResult>(API_URL + `/lendables/${id}?format=json`);
+	return getSingle<LendablesResult>(`lendables/${id}`);
 }
--- frontend/src/routes/tools/[id]/+page.svelte
+++ frontend/src/routes/tools/[id]/+page.svelte
@@ -1,58 +1,85 @@
 <script>
-	import Title from "@/components/type/title.svelte";
+	import Title from '@/components/type/title.svelte';
 
-  export let data;
+	export let data;
 </script>
-
-<style>
-  dt, dd {
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: .75em;
-    padding: 0;
-    margin: 0 0 1em 0;
-  }
-  dt {
-    text-transform: uppercase;
-    font-size: .5em;
-    opacity: .5;
-  }
-  .grid {
-    display: grid;
-  }
-  .grid.center {
-    justify-content: center;
-    align-items: center;
-  }
-  .grid-8em {
-    grid-template-columns: repeat(auto-fit, minmax(8em, 1fr));
-  }
-  .grid-item {
-    grid-column: span 1;
-    grid-row: span 1;
-  }
-  .grid.center .grid-item {
-    text-align: center;
-  }
-</style>
 
 <Title><h1>{data.tool.name}</h1></Title>
 <dl class="grid grid-8em center">
-  {#if data.location}
-  <div class="grid-item">
-    <dt>Location</dt>
-    <dd>{data.location.name}</dd>
-  </div>
-  {/if}
-  {#if data.owner}
-  <div class="grid-item">
-    <dt>Owner</dt>
-    <dd>{data.owner.username}</dd>
-  </div>
-  {/if}
-  {#if data.status}
-  <div class="grid-item">
-    <dt>Status</dt>
-    <dd>{data.status.name}</dd>
-  </div>
-  {/if}
+	{#if data.location}
+		<div class="grid-item">
+			<dt>Location</dt>
+			<dd>{data.location.name}</dd>
+		</div>
+	{/if}
+	{#if data.owner}
+		<div class="grid-item">
+			<dt>Owner</dt>
+			<dd>{data.owner.username}</dd>
+		</div>
+	{/if}
+	{#if data.status}
+		<div class="grid-item">
+			<dt>Status</dt>
+			<dd>{data.status.name}</dd>
+		</div>
+	{/if}
 </dl>
+{#if data.history}
+	<h2>History</h2>
+	<table class="loan-history">
+		<thead>
+			<tr>
+				<td>Loaned To</td>
+				<td>Loaned On</td>
+				<td>Returned On</td>
+			</tr>
+		</thead>
+		{#each data.history.results as loan}
+			<tr>
+				<td>{loan.borrowing_user_data?.username}</td>
+				<td>{new Date(loan.start_date).toDateString()}</td>
+				<td>{new Date(loan.returned_datetime).toDateString()}</td>
+			</tr>
+		{/each}
+	</table>
+{/if}
+
+<style>
+	dt,
+	dd {
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		font-size: 0.75em;
+		padding: 0;
+		margin: 0 0 1em 0;
+	}
+	dt {
+		text-transform: uppercase;
+		font-size: 0.5em;
+		opacity: 0.5;
+	}
+	.grid {
+		display: grid;
+	}
+	.grid.center {
+		justify-content: center;
+		align-items: center;
+	}
+	.grid-8em {
+		grid-template-columns: repeat(auto-fit, minmax(8em, 1fr));
+	}
+	.grid-item {
+		grid-column: span 1;
+		grid-row: span 1;
+	}
+	.grid.center .grid-item {
+		text-align: center;
+	}
+	.loan-history td {
+		padding: 0.5em;
+	}
+	.loan-history tr {
+		border: 1px solid #ccc;
+	}
+</style>
