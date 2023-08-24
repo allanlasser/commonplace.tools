b6990aefaf56914c9a218769efbabbc637e294c6
Chris Hartley <chris.hartley@renewindianapolis.org>
2023-08-24 09:43:15 -0400

    remove keys

--- initialize_dotenv.py
+++ initialize_dotenv.py
@@ -21,15 +21,12 @@ PGPASSWORD = random_string(60)
 SECRET_KEY = 'django-insecure-eyiudt)c4efbabjqea@k^+od%p0a@rmw+v6%ys9+dn0tq%z_ga'
 DEBUG = True
 ALLOWED_HOSTS = ''
-LETSENCRYPT_EMAIL='chris.hartley@anymouse.org'
-LETSENCRYPT_HOST='www.commonplace.tools'
+LETSENCRYPT_EMAIL=''
+LETSENCRYPT_HOST=''
 POSTGRES_NAME='postgres'
 POSTGRES_USER='postgres'
 POSTGRES_PASSWORD='postgres'
-AWS_S3_ACCESS_KEY_ID = 'AKIA3TSVFRSM2C7PHIDG'
-AWS_S3_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'media.commonplace.tools'
-AWS_S3_REGION_NAME = 'us-east-2'
-
-with open('.env', "w") as file:
-    
+AWS_S3_ACCESS_KEY_ID = ''
+AWS_S3_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_S3_REGION_NAME = ''
